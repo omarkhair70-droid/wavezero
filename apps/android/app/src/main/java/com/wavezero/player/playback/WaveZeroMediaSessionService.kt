395051772b2081cecd5159aed2643ea286d85f76
omarkhair70-droid <omar.khair70@gmail.com>
2026-05-29 16:42:03 +0300

    Fix foreground media service import

--- apps/android/app/src/main/java/com/wavezero/player/playback/WaveZeroMediaSessionService.kt
+++ apps/android/app/src/main/java/com/wavezero/player/playback/WaveZeroMediaSessionService.kt
@@ -4,8 +4,8 @@ import android.app.Notification
 import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.app.PendingIntent
-import android.app.ServiceInfo
 import android.content.Intent
+import android.content.pm.ServiceInfo
 import android.os.Build
 import androidx.annotation.OptIn
 import androidx.media3.common.util.UnstableApi
