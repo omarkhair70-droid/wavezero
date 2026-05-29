b0b7c2598628fd0fda22c3b4509c24645800a104
omarkhair70-droid <omar.khair70@gmail.com>
2026-05-29 12:53:51 +0300

    Use standard Flutter Android build output directory

--- apps/flutter/wavezero_app/android/build.gradle.kts
+++ apps/flutter/wavezero_app/android/build.gradle.kts
@@ -1,4 +1,20 @@
+import org.gradle.api.file.Directory
+
 plugins {
     id("com.android.application") apply false
     id("org.jetbrains.kotlin.android") apply false
+}
+
+// Keep the Flutter Android host build output in the location that `flutter run`
+// expects: apps/flutter/wavezero_app/build/... instead of android/app/build/...
+val newBuildDir: Directory = rootProject.layout.buildDirectory.dir("../../build").get()
+rootProject.layout.buildDirectory.value(newBuildDir)
+
+subprojects {
+    val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
+    project.layout.buildDirectory.value(newSubprojectBuildDir)
+}
+
+subprojects {
+    project.evaluationDependsOn(":app")
 }
