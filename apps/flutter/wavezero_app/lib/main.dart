1f149596984b700929ce6913cdad014297e117ea
omarkhair70-droid <omar.khair70@gmail.com>
2026-05-29 17:27:49 +0300

    Use live-refreshing metrics app entrypoint

--- apps/flutter/wavezero_app/lib/main.dart
+++ apps/flutter/wavezero_app/lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
-import 'app/wavezero_app.dart';
+import 'app/wavezero_live_metrics_app.dart';
 
 void main() {
-  runApp(const WaveZeroApp());
+  runApp(const WaveZeroLiveMetricsApp());
 }
