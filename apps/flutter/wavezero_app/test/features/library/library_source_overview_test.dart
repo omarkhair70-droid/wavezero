11472ec34c05a015d78a2e1150f757a6af58436d
Omar Khair <omar.khair70@gmail.com>
2026-09-15 23:05:15 +0300

    Fix async Music Inbox callback test contract

--- apps/flutter/wavezero_app/test/features/library/library_source_overview_test.dart
+++ apps/flutter/wavezero_app/test/features/library/library_source_overview_test.dart
@@ -24,7 +24,7 @@ void main() {
               onSourceFilterChanged: onFilterChanged ?? (_) {},
               onRefresh: () {},
               onImportDeviceMusic: () async {},
-              onOpenMusicInbox: () {},
+              onOpenMusicInbox: () async {},
               onOpenCollections: () {},
               onOpenFullSearch: () {},
               onOpenCloudVault: () {},
