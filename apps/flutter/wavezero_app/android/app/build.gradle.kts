1aaff67a83bb66a089dd8cf7466f5d5a2a0f0895
Omar Khair <omar.khair70@gmail.com>
2026-09-16 01:04:11 +0300

    Add Media3 Transformer for local video audio extraction

--- apps/flutter/wavezero_app/android/app/build.gradle.kts
+++ apps/flutter/wavezero_app/android/app/build.gradle.kts
@@ -78,5 +78,6 @@ dependencies {
     implementation("androidx.media3:media3-exoplayer-hls:$media3Version")
     implementation("androidx.media3:media3-datasource:$media3Version")
     implementation("androidx.media3:media3-session:$media3Version")
+    implementation("androidx.media3:media3-transformer:$media3Version")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.9.0")
 }
